6126d86267d7a16525fd597170bfc6035c41c815
Gamze Güneri <gamzegunerig@gmail.com>
2021-08-11 20:40:39 +0300

    redux files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import { Route, Link } from "react-router-dom";
+import { BrowserRouter , Route , Link} from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Col, Container, Row } from "react-bootstrap";
+import Navbar from '../src/components/navbar/Navbar'
 
 
 
 function App() {
   return (
 
-    <Router>
-      <Container>
-      <Row>
-        <Col md={12} className="text-center">
-            <h1>Get A Job</h1>
-        </Col>
-        
-      </Row>
-      <hr/>
-      </Container>
-    </Router>
+   <Navbar/>
   );
 }
 
